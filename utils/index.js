715520e8dc862884109b079d61feca59d67d7331
itzjustfitb <fakhrigajar@gmail.com>
2024-07-07 12:48:42 +0400

    cart sidebar was added

--- utils/index.js
+++ utils/index.js
@@ -14,3 +14,24 @@ sectionProducts.forEach((item, i) => {
     item.scrollLeft -= containerWidth;
   });
 });
+
+const cartBtn = document.querySelector("#cart-icon");
+const cartMenu = document.querySelector(".subheader__basket");
+const cartRemove = document.querySelector("#subheader__basket-remove");
+const cartContainer = document.querySelector(".subheader__basket");
+
+cartRemove.addEventListener("click", () => {
+  cartMenu.classList.remove("basket-active");
+});
+
+document.body.addEventListener("click", () => {
+  cartMenu.classList.remove("basket-active");
+});
+cartBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  cartMenu.classList.toggle("basket-active");
+});
+
+cartContainer.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
